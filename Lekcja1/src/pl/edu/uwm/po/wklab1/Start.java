6f657514f9c9b18a5d9289ae8e3dd592a1a03b0b
Krejnerw <weronika.krejner@hotmail.com>
2020-10-05 17:09:40 +0200

    L1 start

---
+++ Lekcja1/src/pl/edu/uwm/po/wklab1/Start.java
@@ -0,0 +1,11 @@
+package pl.edu.uwm.po.wklab1;
+
+public class Start {
+    public static void main(String[] args){
+        System.out.println("StartApp is running...");
+        /**
+         * The HelloWorldApp class implements an application that
+         * simply prints "Hello World!" to standard output.
+         */
+    }
+}
