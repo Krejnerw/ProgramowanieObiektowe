e5a4c6b3fd970c6208f21c01034850a1fff3b662
Krejnerw <weronika.krejner@hotmail.com>
2020-10-06 18:36:15 +0200

    ZAD3_8

---
+++ Lekcja1/src/pl/edu/uwm/po/wklab1/ZAD3_8.java
@@ -0,0 +1,9 @@
+package pl.edu.uwm.po.wklab1;
+
+public class ZAD3_8 {
+    public static void main(String[] args){
+        System.out.println("   +\n  + +\n +   +");
+        System.out.println("+"+"-".repeat(5)+"+");
+        System.out.println("| .-. |\n| | | |\n+-+-+-+");
+    }
+}
