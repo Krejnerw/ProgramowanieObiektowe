b0159f304402f5d7bad1a308667a6671e0166033
Krejnerw <weronika.krejner@hotmail.com>
2020-10-06 18:36:02 +0200

    ZAD3_6

---
+++ Lekcja1/src/pl/edu/uwm/po/wklab1/ZAD3_6.java
@@ -0,0 +1,25 @@
+package pl.edu.uwm.po.wklab1;
+
+public class ZAD3_6 {
+    public static void extraLudek(){
+        System.out.println("  "+"/".repeat(5)+"  ");
+        System.out.println(" +"+"\"".repeat(5)+"+ ");
+        System.out.println("(| o o |)");
+        System.out.println("  | ^ |  ");
+        System.out.println("   ___  ");
+        System.out.println("\\   |   /");
+        System.out.println(" \\_____/ ");
+        System.out.println(" | ._. | ");
+        System.out.println(" +"+"-".repeat(5)+"+ ");
+        System.out.println("  |   |  ");
+    }
+    public static void main(String[] args){
+        System.out.println("  "+"/".repeat(5)+"  ");
+        System.out.println(" +"+"\"".repeat(5)+"+ ");
+        System.out.println("(| o o |)");
+        System.out.println("  | ^ |  ");
+        System.out.println(" | ._. | ");
+        System.out.println(" +"+"-".repeat(5)+"+ ");
+        //extraLudek();
+    }
+}
